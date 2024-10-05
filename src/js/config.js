2c66d9aa36ff40358313a2f6fb314c50bd3fe963
ShahdRaafat <shahdraafat.725@gmail.com>
2024-10-06 00:32:17 +0300

    Final

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const SPOONACULAR_URL = 'https://api.spoonacular.com/food/ingredients/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '3d0b0e97-970d-4494-adbf-0d937321c1a2';
+export const KEY = '6747b1ad-79ac-4e71-8909-83186c7567e1';
 export const SPOONACULAR_KEY = '9b8f374af5ff424ba1131e2a031604f8';
 export const CLOSE_MODAL_SEC = 2.5;
